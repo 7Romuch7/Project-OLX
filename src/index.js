f31d81038d5dd2b6027817c972340128cc0e4118
mitrofin <mitrofn@gmail.com>
2020-12-10 02:51:31 +0200

    Create modal

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import './styles.scss';
 import './scripts/headers.js';
 import './scripts/footer';
-import './scripts/add-modal';
+/* import './scripts/add-modal'; */
 import './scripts/menu.js';
+import './scripts/addProductModal.js';
---
+++ src/scripts/addProductModal.js
@@ -0,0 +1,27 @@
+import productModal from '../templates/productModal.hbs'
+
+const modalProductContainer = document.querySelector('.footer');
+const openModalProduct = document.querySelector('[data-modal_product]');
+const closeModal = document.querySelector('[close_modal_product]');
+
+modalProductContainer.addEventListener('click', openModalFunc);
+closeModal.addEventListener('click', closeModalProduct);
+
+
+function openModalFunc() {
+    /* evt.preventDefault(); */
+    /* openModalProduct.classList.remove('is-hidden') */
+    modalAppend()
+}
+
+function closeModalProduct() {
+   /* openModalProduct.classList.add('is-hidden')  */
+   modalProductContainer.insertAdjacentHTML("")
+}
+
+
+function modalAppend() {
+    modalProductContainer.insertAdjacentHTML('afterbegin', productModal())
+    /* modalProductContainer.classList.add('is-hidden') */ 
+}
+/* modalAppend() */

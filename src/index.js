696a979773a033c39578d37505a688cfbb08cd31
Lyudmyla Boiko <itsforyou.com.ua@gmail.com>
2020-12-10 21:19:56 +0200

    resolve conflicts?

--- src/index.js
+++ src/index.js
@@ -1,3 +1,8 @@
+  
 import './styles.scss';
 import './scripts/headers.js';
+import './scripts/footer.js';
+import './scripts/add-modal.js';
+import './scripts/menu.js';
+import './scripts/reg-modal.js';
 import './scripts/searchModal';

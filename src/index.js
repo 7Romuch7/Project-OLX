e0743c269c2c3627ee68ab1b0a86591acc482ce1
Lyudmyla Boiko <itsforyou.com.ua@gmail.com>
2020-12-12 18:53:27 +0200

    try2

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ import './scripts/footer.js';
 import './scripts/add-modal.js';
 import './scripts/menu.js';
 import './scripts/reg-modal.js';
-import './scripts/searchModal.js';
 import './scripts/add-modal-open-close';
+import './scripts/searchModal.js';
 import './scripts/searchModalRender.js';
 

f12690ee29dd4b8429a9165154016de40cc8efa7
mitrofin <mitrofn@gmail.com>
2020-12-13 01:20:45 +0200

    fexed

--- src/scripts/addProductModal.js
+++ src/scripts/addProductModal.js
@@ -1,39 +1,39 @@
 
 import productModal from '../templates/productModal.hbs';
 
-
-
 const modalProductContainer = document.querySelector("header");
-const openModalProduct = document.getElementById("myBtn");
-const closeModal = document.querySelector('close_modal_product');
-
-
-
-
-/* closeModal.addEventListener('click', closeModalProduct); */
-openModalProduct.addEventListener('click', openModalFunc);
-
-
-function openModalFunc(evt) {
-    evt.preventDefault();
-    /* modalProductContainer.classList.toggle('is-hidden') */
-    modalAppend()
-    window.addEventListener('keydown', onEscKeydown);
-
-}
-
-function closeModalProduct() {
-    modalProductContainer.classList.add('is-hidden');
-    modalProductContainer.insertAdjacentHTML = ''
-    
-}
-
 
 function modalAppend() {
     modalProductContainer.insertAdjacentHTML('afterbegin', productModal())
      
 }
-/* modalAppend() */
+modalAppend()
+
+const openModalProduct = document.getElementById("myBtn");
+const closeModal = document.querySelector('#close_modal_product');
+const backdrop = document.querySelector('.backdropProduct');
+
+
+
+
+closeModal.addEventListener('click', closeModalProduct);
+openModalProduct.addEventListener('click', openModalFunc);
+
+
+function openModalFunc(evt) {
+    evt.preventDefault();
+    backdrop.classList.remove('is-hidden')
+    window.addEventListener('keydown', onEscKeydown);
+
+}
+
+function closeModalProduct() {
+    backdrop.classList.add('is-hidden')
+    
+}
+
+
+
 function onEscKeydown(event) {
     if (event.code === 'Escape') {
        closeModalProduct()

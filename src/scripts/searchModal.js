01cc9e39a5c3600f5f0f460abc60052d664f1329
Lyudmyla Boiko <itsforyou.com.ua@gmail.com>
2020-12-12 16:10:20 +0200

    open-close work

--- src/scripts/searchModal.js
+++ src/scripts/searchModal.js
@@ -1,18 +1,20 @@
 
 const backdrop = document.querySelector('.search-modal-backdrop');
-const openSearchModalBtn = document.querySelector('.search-btn');
-const closeSearchModalBtn = document.querySelector('.close-search-modal-button');
+const openSearchModalBtn = document.querySelector('#search-btn');
+const closeSearchModalBtn = document.querySelector('#close-search-modal-button');
 
-openSearchModalBtn.addEventListener("click", openSearchModal);
-// closeSearchModalBtn.addEventListener("click", closeSearchModal);
-console.log('work search modal.js');
+openSearchModalBtn.addEventListener('click', openSearchModal);
+closeSearchModalBtn.addEventListener("click", closeSearchModal);
 
 function openSearchModal(evt) {
     evt.preventDefault()
     backdrop.classList.remove('is-hidden')
-    console.log('work searchbutton')
 }
-// function closeSearchModal() {
+   
+function closeSearchModal(evt) { 
+    evt.preventDefault()
+    backdrop.classList.add('is-hidden')
+}
     
 
 

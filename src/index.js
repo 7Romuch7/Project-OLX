06e2d4bd90d0a93983e400a4c592793799bc34f7
Lyudmyla Boiko <itsforyou.com.ua@gmail.com>
2020-12-12 17:51:14 +0200

    индекс js  вернула

--- src/index.js
+++ src/index.js
@@ -1,9 +1,9 @@
 import './styles.scss';
 import './scripts/headers.js';
 import './scripts/footer.js';
-// import './scripts/add-modal.js';
+import './scripts/add-modal.js';
 import './scripts/menu.js';
 import './scripts/reg-modal.js';
-// import './scripts/add-modal-open-close';
+import './scripts/add-modal-open-close';
 import './scripts/searchModalRender.js';
 import './scripts/searchModal.js';

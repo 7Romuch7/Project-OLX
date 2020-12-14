11cf9a285fd2f1ec9e4e61b6ff11cf0afe503b2a
7Romuch7 <64313145+7Romuch7@users.noreply.github.com>
2020-12-14 23:04:04 +0200

    некоторые наработки

--- src/scripts/reg-modal.js
+++ src/scripts/reg-modal.js
@@ -4,11 +4,13 @@ import regModal from '../templates/reg-modal.hbs'
 
 const regModalRef = document.querySelector('.lightbox__content')
 const openModalBtn = document.querySelector('.login__btn')
+const openUserProfile = document.querySelector('.user__profile-header')
 const modalEl = document.querySelector('.js-lightbox')
 const closeModalBtn = document.querySelector('.close-reg-button')
 
 
 openModalBtn.addEventListener('click', openModal)
+openUserProfile.addEventListener('click', openModal)
 closeModalBtn.addEventListener('click', closeModal)
 
 function openModal(evt) {

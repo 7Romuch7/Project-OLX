a97b3b8497d5ac71c9f1307a8555b2d7310a22ca
Bohdan Boiko <bogdanprogr@gmail.com>
2020-12-20 20:55:16 +0200

    gallery-slider

--- src/scripts/gallery/card-gallery.js
+++ src/scripts/gallery/card-gallery.js
@@ -2,10 +2,7 @@ import templatesCadrGallery from '../../templates/cardGallery.hbs';
 import templatesCategory from '../../templates/category.hbs';
 import { addSlider } from './slider.js';
 import { BASE_URL } from '../../scripts/constans/index.js'
-import { Object } from 'core-js';
-import openModalProductCard from '../product_modal/addProductModal';
-
-const cardGallaryMarkup = document.querySelector('.gallery');
+// import { Object } from 'core-js';
 const cardCategoryMarkup = document.querySelector('.main');
 const translationCategory = {
         sales: 'Розпродаж',
@@ -18,37 +15,36 @@ const translationCategory = {
         free: 'Віддам безкоштовно',
         trade: 'Обмін',
 }
-function renderCard(categories) { 
-  for (let card in categories) { 
-    let gellary = document.querySelector('[data-category="'+card+'"]');
-    gellary.insertAdjacentHTML('beforeend', templatesCadrGallery(categories[card]));
 
-    for (let i in categories[card]) {
-      let cardData = categories[card][i];
-      let cardDomObject = gellary.querySelector('[data-card-index="' + i + '"]');
-      cardDomObject.addEventListener('click', function (e) {
-        openModalProductCard(cardData)
-      });
-    }
-  }
-  
-}
-function fetchCategories() { 
+  function fetchCategories() { 
   fetch(`${BASE_URL}/call/?page=1`)
     .then(r => r.json())
     .then(categories => {
-      let items = Object.keys(categories).map( item => {
-        return {
+      let items = Object.keys(categories).map(item => {         
+            return {
           category: item,
-          title: translationCategory[item]
-        }
-        
-      }) 
-      let arrCategories = templatesCategory( items );
-      cardCategoryMarkup.insertAdjacentHTML('beforeend', arrCategories);
-      renderCard(categories);
-      /* console.log(categories); */
+          title: translationCategory[item],
+        }      
+    }); 
       
+      let arrCategories = templatesCategory(items);
+      cardCategoryMarkup.insertAdjacentHTML('beforeend', arrCategories);  
+      
+      renderCard(categories);        
     })
-}
+  }
+
+ function renderCard(categories) { 
+   for (let card in categories) {       
+    let gellary = document.querySelector('[data-category="' + card + '"]');    
+     gellary.insertAdjacentHTML('beforeend', templatesCadrGallery(categories[card]));
+        
+   }
+    const cardGallery = document.querySelectorAll('.gallery')
+      cardGallery.forEach(item => { 
+        addSlider(item);       
+      });  
+    }
+    
 fetchCategories();
+
--- src/scripts/gallery/slider.js
+++ src/scripts/gallery/slider.js
@@ -1,36 +1,36 @@
-export  const addSlider = () => {
-    const viewport = document.getElementById("viewport").offsetWidth;
-    const btnNext = document.getElementById("next");
-    const btnPrev = document.getElementById("prev");
-    const slider = document.querySelector("div.slider");
-    const viewSliders = document.querySelectorAll(".viewSlide");
+export  const addSlider = (item) => {
+    const viewport = item.querySelector(".viewport").offsetWidth;
+    const btnNext = item.querySelector(".next");
+    const btnPrev = item.querySelector(".prev");
+    const slider = item.querySelector("div.slider");
+    // const viewSliders = item.querySelectorAll(".viewSlide");
     let viewSlide = 0; 
 
-    viewSliders[0].style.backgroundColor = "green"; 
 
-    btnNext.addEventListener("click", function () {    
-        viewSliders[viewSlide].style.backgroundColor = "red";
+    btnNext.addEventListener("click",  function () {    
+
         
         if (viewSlide < 4) {         
             viewSlide++;
         } else {         
             viewSlide = 0;
         }
-        
-        viewSliders[viewSlide].style.backgroundColor = "green";    
+
         slider.style.left = -viewSlide * viewport + "px";
     });
     
 
     btnPrev.addEventListener("click", function () {
         
-        viewSliders[viewSlide].style.backgroundColor = "red";    
+        
         if (viewSlide > 0) {         
             viewSlide--; 
         } else {         
             viewSlide = 4; 
         }    
-        viewSliders[viewSlide].style.backgroundColor = "green";    
+
         slider.style.left = -viewSlide * viewport + "px";
     });
+
 }
+

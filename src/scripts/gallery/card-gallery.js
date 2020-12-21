659ba11119c7dfa92e989ce152e507b0abd0149d
7Romuch7 <64313145+7Romuch7@users.noreply.github.com>
2020-12-21 19:16:53 +0200

    преобразование

--- src/scripts/gallery/card-gallery.js
+++ src/scripts/gallery/card-gallery.js
@@ -2,8 +2,8 @@ import templatesCadrGallery from '../../templates/cardGallery.hbs';
 import templatesCategory from '../../templates/category.hbs';
 import { addSlider } from './slider.js';
 import { BASE_URL } from '../constans/index.js'
-import { Object } from 'core-js';
-// const cardGallaryMarkup = document.querySelector('.gallery');
+// import { Object } from 'core-js';
+// const cardGallery = document.querySelector('.gallery');
 const cardCategoryMarkup = document.querySelector('.gallery-markup');
 const translationCategory = {
         sales: 'Розпродаж',
@@ -23,6 +23,17 @@ const pagContainer = document.querySelector('.active-radio-btn');
 pagContainer.addEventListener('click', radioBtnChoice);
 cardCategoryMarkup.addEventListener('click', showAllGoodsCategory)
 
+/*=========Функция загрузки данных выбранной категории=========*/
+function getDataCategory(category) { 
+  fetch(`${BASE_URL}/call/specific/${category}`)
+    .then(response => response.json())
+    .then(result => {
+        console.log(result);
+        renderCardFullCategory(result);
+      })
+    .catch(error => console.log('error', error));
+};
+
 /*=========Функция call-back для клика на "Дивитись усі"=========*/
 function showAllGoodsCategory(event) { 
   console.log(event.target.dataset.filter);
@@ -30,21 +41,6 @@ function showAllGoodsCategory(event) {
   getDataCategory(event.target.dataset.filter);  
   }
 }
-
-/*=========Функция загрузки данных выбранной категории=========*/
-function getDataCategory(category) { 
-  fetch(`${BASE_URL}/call/specific/${category}`)
-    .then(
-      response => response.json()
-    )
-    .then(
-      result => {
-        console.log(result);
-        renderCardFullCategory(result);
-      }
-    )
-    .catch(error => console.log('error', error));
-};
 
 
 /*=========Функция первой загрузки страницы сайта=========*/
@@ -72,14 +68,29 @@ function clearGallery() {
 /*=========Функция рендеринга карточки товара=========*/
 function renderCard(categories) { 
   for (let card in categories) { 
+    console.log('this is card', card);
+    console.log('this is categories card', categories[card]);
     let gellary = document.querySelector('[data-category="'+card+'"]');
     gellary.insertAdjacentHTML('beforeend', templatesCadrGallery(categories[card]))
+     
   }
-
-  const cardGallery = document.querySelectorAll('.gallery')
+  const cardGallery = document.querySelectorAll('.gallery');
       cardGallery.forEach(item => { 
         addSlider(item);       
-      });  
+      });
+  // let startSlider = async (arr) => { 
+  //   arr.forEach(async (item) => {
+  //     await addSlider(item);
+  //   }); 
+    // startSlider(cardGallery)
+  // }
+  // async function startSlider(arr) {
+  //   arr.forEach(async (item) => {
+  //     await addSlider(item);
+  //   });    
+  //   startSlider(cardGallery);
+  // }  
+  // const cardGallery = document.querySelectorAll('.gallery')    
 }
 
 /*=========Функция рендеринга всех товаров данной категории=========*/
@@ -88,28 +99,38 @@ function renderCardFullCategory(gallery) {
   cardCategoryMarkup.insertAdjacentHTML('beforeend', templatesCadrGallery(gallery));
 }
 
-
 /*=========Функция запроса на АПИ на категории=========*/
 function fetchCategories(pageNum) { 
-  fetch(`${BASE_URL}/call/?page=${pageNum}`)
+  fetch(`${BASE_URL}/call/?page=${pageNum}`)    
     .then(r => r.json())
     .then(categories => {
-      let items = Object.keys(categories).map( item => {
+      let items = Object.keys(categories).map(item => {
         return {
           category: item,
           title: translationCategory[item]
         }
-      }) 
-      let arrCategories = templatesCategory( items );
+      })
+      let arrCategories = templatesCategory(items);
       cardCategoryMarkup.insertAdjacentHTML('afterbegin', arrCategories);
-     
+      console.log('this is numPage', pageNum);
+      console.log('this is categories', categories);
       renderCard(categories);
       // renderPaginationBtns();
-    })
+    });
 }
+
+// const cardGallery = document.querySelectorAll('.gallery')
+
+//   async function processArray(arr) {
+//   for (const item of arr) {
+//     await addSlider(item);
+//   } 
+// }
+// processArray(cardGallery)
 
 
 firstLoad();
+
 
 
 
--- src/scripts/gallery/slider.js
+++ src/scripts/gallery/slider.js
@@ -1,81 +1,12 @@
-// const viewport = document.getElementById("viewport").offsetWidth;
-// const btnNext = document.getElementById("next");
-// const btnPrev = document.getElementById("prev");
-// const slider = document.querySelector("div.slider");
-// const viewSliders = document.querySelectorAll(".viewSlide");
-// let viewSlide = 0; 
-
-// viewSliders[0].style.backgroundColor = "green"; 
-
-// btnNext.addEventListener("click", function () {    
-//     viewSliders[viewSlide].style.backgroundColor = "red";
-    
-//     if (viewSlide < 4) {         
-//         viewSlide++;
-//     } else {         
-//         viewSlide = 0;
-//     }
-    
-//     viewSliders[viewSlide].style.backgroundColor = "green";    
-//     slider.style.left = -viewSlide * viewport + "px";
-// });
- 
-
-// btnPrev.addEventListener("click", function () {
-    
-//     viewSliders[viewSlide].style.backgroundColor = "red";    
-//     if (viewSlide > 0) {         
-//         viewSlide--; 
-//     } else {         
-//         viewSlide = 4; 
-//     }    
-//     viewSliders[viewSlide].style.backgroundColor = "green";    
-//     slider.style.left = -viewSlide * viewport + "px";
-// });
-
-// 
-// export  const addSlider = (item) => {
-//     const viewport = item.querySelector(".viewport").offsetWidth;
-//     const btnNext = item.querySelector(".next");
-//     const btnPrev = item.querySelector(".prev");
-//     const slider = item.querySelector("div.slider");
-//     // const viewSliders = item.querySelectorAll(".viewSlide");
-//     let viewSlide = 0; 
-
-
-//     btnNext.addEventListener("click",  function (e) {    
-//         e.preventDefault();
-        
-//         if (viewSlide < 1) {         
-//             viewSlide++;
-//         } else {         
-//             viewSlide = 0;
-//         }
-
-//         slider.style.left = -viewSlide * viewport + "px";
-//     });
-    
-
-//     btnPrev.addEventListener("click", function () {
-        
- 
-//         if (viewSlide > 0) {         
-//             viewSlide--; 
-//         } else {         
-//             viewSlide = 1; 
-//         }    
-
-//         slider.style.left = -viewSlide * viewport + "px";
-//     });
-// }
-
 export  const addSlider = (item) => {
-    const viewport = item.querySelector(".viewport").offsetWidth;
+    // // const viewSliders = item.querySelectorAll(".viewSlide");
+    let viewportRef = item.querySelector(".viewport");
+    if (!viewportRef) return
+    const viewport = viewportRef.offsetWidth;
     const btnNext = item.querySelector(".next");
     const btnPrev = item.querySelector(".prev");
     const slider = item.querySelector("div.slider");
-    // const viewSliders = item.querySelectorAll(".viewSlide");
-    let viewSlide = 0; 
+    let viewSlide = 0;
 
 
     btnNext.addEventListener("click",  function () {    
@@ -104,6 +35,4 @@ export  const addSlider = (item) => {
     });
 
 }
-
-
 

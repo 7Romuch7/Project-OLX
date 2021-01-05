6423fd615909a5c3f99e18ac968eb5719ef264ef
7Romuch7 <64313145+7Romuch7@users.noreply.github.com>
2021-01-05 20:24:45 +0200

    Токен сохранен

--- src/scripts/reg_modal/fetchLogInForm.js
+++ src/scripts/reg_modal/fetchLogInForm.js
@@ -13,27 +13,30 @@ export default class FetchLogIn {
                 body: JSON.stringify({"email": `${email}`, "password": `${password}`}),
             };
         }
-            getCurrentUserData() {
-                return fetch(`https://callboard-backend.herokuapp.com/auth/login`, this.options)
-                    .then(response => {
-                        if (response.ok) {
-                           // refs.authFormContainer.innerHTML =' ';
-                            localStorage.setItem('token', response.accessToken);
-                            localStorage.setItem('sid', response.sid);
-                            localStorage.setItem('refresh', response.refreshToken);
-                            console.log(response.user);
-                        } else if (response.status === 403) {
-                            alert('Ошибка HTTP ' + result.status + ': Неправильний email або пароль');
-                        } else if (response.status === 400) {
-                            alert('Ошибка HTTP ' + response.status + ': Заповніть, будь ласка, всі поля.');
-                        } else {
-                            alert('Ошибка HTTP ' + response.status + ': Вибачте, щось пішло не так, повторіть реєстрацію, будь ласка.');
-                        }    
-                    }
-                )
-                .catch(error => {
+    getCurrentUserData() {
+        return fetch(`https://callboard-backend.herokuapp.com/auth/login`, this.options)
+            .then(response => {
+                if (response.ok) {
+                    return response.json()
+                }
+            })
+            .then(response => {
+                    // refs.authFormContainer.innerHTML =' ';
+                    localStorage.setItem('token', response.accessToken);
+                    localStorage.setItem('sid', response.sid);
+                    localStorage.setItem('refresh', response.refreshToken);
+                    console.log(response.user);
+                if (response.status === 403) {
+                    alert('Ошибка HTTP ' + result.status + ': Неправильний email або пароль');
+                } else if (response.status === 400) {
+                    alert('Ошибка HTTP ' + response.status + ': Заповніть, будь ласка, всі поля.');
+                } else {
+                alert('Ошибка HTTP ' + response.status + ': Вибачте, щось пішло не так, повторіть реєстрацію, будь ласка.');
+                }
+            })
+            .catch(error => {
                     console.log(error);
                     alert('Вибачте, щось пішло не так, повторіть реєстрацію, будь ласка');
                 });
-            }
-        }
+    }
+}
